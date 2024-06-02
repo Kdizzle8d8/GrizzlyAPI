114beed105d21dbc68f3a1db496c36b7aa5b9bf1
Matthew Davis <matthew@matthewdavis.io>
2024-06-02 16:58:58 -0500

    bump

--- calendars/dates.go
+++ calendars/dates.go
@@ -4,27 +4,28 @@ import (
 	"fmt"
 	"time"
 )
+
 type dateResult struct {
 	Date string `json:"date"`
 	Type string `json:"type"`
 }
 
-func GetTypeFromDate(month int, day int) dateResult {
+func GetTypeFromDate(month int, day int) (dateResult, error) {
 	c := NewCalendar()
 
 	for _, m := range c.Months {
 		if m.Number == month {
 			for _, d := range m.Days {
 				if d.Number == day {
-					return dateResult{Date: fmt.Sprintf("%d/%d", month, day), Type: d.Type} 
+					return dateResult{Date: fmt.Sprintf("%d/%d", month, day), Type: d.Type}, nil
 				}
 			}
 		}
 	}
-	return dateResult{} // Return empty dateResult if no match is found
+	return dateResult{}, fmt.Errorf("date not found")
 }
-func GetTypeFromToday() dateResult  {
-	day:=time.Now().Day()
-	month:=int(time.Now().Month())
-	return GetTypeFromDate(month,day)
+func GetTypeFromToday() (dateResult, error) {
+	day := time.Now().Day()
+	month := int(time.Now().Month())
+	return GetTypeFromDate(month, day)
 }
