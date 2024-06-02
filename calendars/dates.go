0a059307b8f35e7dca2811c8be032431d8bc071b
Kyle Davis <kyle@kyle.stream>
2024-06-02 17:13:29 -0500

    Removed redundant endpoints, cleaned up code, added error checking

--- calendars/dates.go
+++ calendars/dates.go
@@ -2,29 +2,24 @@ package calendars
 
 import (
 	"fmt"
-	"time"
 )
+
 type dateResult struct {
 	Date string `json:"date"`
 	Type string `json:"type"`
 }
 
-func GetTypeFromDate(month int, day int) dateResult {
+func GetTypeFromDate(month int, day int) (dateResult, error) {
 	c := NewCalendar()
 
 	for _, m := range c.Months {
 		if m.Number == month {
 			for _, d := range m.Days {
 				if d.Number == day {
-					return dateResult{Date: fmt.Sprintf("%d/%d", month, day), Type: d.Type} 
+					return dateResult{Date: fmt.Sprintf("%d/%d", month, day), Type: d.Type}, nil
 				}
 			}
 		}
 	}
-	return dateResult{} // Return empty dateResult if no match is found
-}
-func GetTypeFromToday() dateResult  {
-	day:=time.Now().Day()
-	month:=int(time.Now().Month())
-	return GetTypeFromDate(month,day)
+	return dateResult{}, fmt.Errorf("Date not found")
 }
--- main.go
+++ main.go
@@ -8,26 +8,23 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func main(){
-	e:=echo.New()
-	e.GET("/",test)
-	e.GET("/date/:month/:day",getDate)
-	e.GET("/date/",func(c echo.Context) error{
-		res:=calendars.GetTypeFromToday()
-		return c.JSON(http.StatusOK,res)	
+func main() {
+	e := echo.New()
+	e.GET("/", test)
+	e.GET("/date/:month/:day", func(c echo.Context) error {
+		month, _ := strconv.Atoi(c.Param("month"))
+		day, _ := strconv.Atoi(c.Param("day"))
+
+		res, err := calendars.GetTypeFromDate(month, day)
+
+		if err != nil {
+			return c.JSON(http.StatusNotFound, map[string]string{"error": err.Error()})
+		}
+
+		return c.JSON(http.StatusOK, res)
 	})
-	
+
 	e.Start(":8080")
-}
-
-func getDate(c echo.Context) error {
-	month, _ := strconv.Atoi(c.Param("month"))
-	day, _ := strconv.Atoi(c.Param("day"))
-
-	res := calendars.GetTypeFromDate(month, day)
-
-
-	return c.JSON(http.StatusOK, res)
 }
 
 func test(c echo.Context) error {
